606e98dfc5f0bb31bc1426c42fe21fbe2dd4e451
Remor53 <43079562+Remor53@users.noreply.github.com>
2020-03-24 23:48:26 +0300

    добавил функции student_change и all_coef_count

--- alldef.py
+++ alldef.py
@@ -61,3 +61,43 @@ def contribution_one_person(person, num_group):
         except:
             pass
     return person, sum_contr
+
+# принимает словарь (ключ - номер группы, значение - множество id cтудентов); возвращает словарь того же формата; производит замены в группах
+# для работы нужен модуль copy и функция all_coef_count
+def student_change(group_dict, is_change = True):
+    while is_change == True:
+        is_change = False
+        for group_num in group_dict:
+            for student_id in group_dict[group_num]:
+                max_coef_change = 0
+                for other_group_num in group_dict:
+                    if other_group_num != group_num:
+                        for other_student_id in group_dict[other_group_num]:
+                            whatif_dict = copy.deepcopy(group_dict)
+                            whatif_dict[group_num].remove(student_id)
+                            whatif_dict[other_group_num].add(student_id)
+                            whatif_dict[other_group_num].remove(other_student_id)
+                            whatif_dict[group_num].add(other_student_id)
+                            sumcoef_before = all_coef_count(group_dict)
+                            sumcoef_after = all_coef_count(whatif_dict)
+                            if sumcoef_after - sumcoef_before > max_coef_change:
+                                max_coef_change = sumcoef_after - sumcoef_before
+                                student_change_id = other_student_id
+                                group_change = other_group_num
+                if max_coef_change > 0:
+                    group_dict[group_num].remove(student_id)
+                    group_dict[group_change].add(student_id)
+                    group_dict[group_change].remove(student_change_id)
+                    group_dict[group_num].add(student_change_id)
+                    is_change = True
+    return group_dict
+
+# принимает словарь (ключ - номер группы, значение - множество id cтудентов); возвращает числовое значение(int), обозначающее сумму коэффициентов отношений во всех группах
+def all_coef_count(group_dict):
+    summcoef = 0
+    for group_number in group_dict:
+        for student_id in group_dict[group_number]:
+            for other_student_id in student_dict1[student_id]:
+                if other_student_id in group_dict[group_number]:
+                    summcoef += student_dict1[student_id][other_student_id]
+    return summcoef
