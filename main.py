c7d15d80b73e16581ef08e4a8bdf15fe2e154e40
pyrogn <2point71828182846@gmail.com>
2020-03-30 23:59:16 +0300

    удаляю ненужное

--- main.py
+++
@@ -1,9 +0,0 @@
-from gen_relationships import full_dict_relations
-from alldef import *
-
-
-# либо же локально загружать этот словарь
-
-list_groups = [[], [], [], [], [], []]
-# full_dict_relations - сгенерированный словарь
-# print(full_dict_relations)
